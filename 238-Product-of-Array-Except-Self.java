e521b45c86fd2223ed2e47f32a2855a97fc1832d
Mukul Gaur <mukulgaur91@gmail.com>
2022-05-15 23:57:02 +0530

    Update 238-Product-of-Array-Except-Self.java

--- 238-Product-of-Array-Except-Self.java
+++ 238-Product-of-Array-Except-Self.java
@@ -1,3 +1,26 @@
+// Solution 2 - RunTime 2ms
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int[] ans = new int[nums.length];
+        int prefix=1, postfix=1;
+        
+        for(int i=0; i<nums.length; i++){
+            ans[i]=prefix;
+            prefix*=nums[i];
+        }
+        
+        for(int i=nums.length-1; i>=0; i--){
+            ans[i]*=postfix;
+            postfix*=nums[i];
+        }
+        
+        return ans;
+        
+    }
+}
+
 //Solution 1 - Time Limit Exceeded for some test cases
 
 class Solution {
