fb383785bfed7ac1c91a1e81c4a03fe342953a13
Mukul Gaur <mukulgaur91@gmail.com>
2022-05-18 16:30:17 +0530

    runtime 2 ms
    Runtime: 2 ms, faster than 99.76% of Java online submissions for Valid Palindrome.
    Memory Usage: 42.3 MB, less than 91.77% of Java online submissions for Valid Palindrome.

--- 125-Valid-Palindrome.java
+++ 125-Valid-Palindrome.java
@@ -1,3 +1,46 @@
+// Solution 2 - Runtime 2 ms
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        int i=0, j=s.length()-1; // two pointers to traverse the String
+        
+        while(i<j){
+            
+            // check if the character at this pointer is Alpha Numeric, if it is not increment the pointer by one.
+            while(i<j && !isAlphaNum(s.charAt(i)))
+                i++;
+            
+            // check if the character at this pointer is Alpha Numeric, if it is not increment the pointer by one.
+            while(j>i && !isAlphaNum(s.charAt(j)))
+                j--;
+            
+            // check if the character are equal, if it's not equal then return false
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) 
+                return false;
+            
+            // increment pointers after every traversal
+            i++;
+            j--;
+            
+        }
+        
+        return true;
+        
+    }
+    
+    // this method will check if the given character is an Alpha Numeric
+    
+    private boolean isAlphaNum(char c){
+        
+        //compare the ASCII value of the character 'c'; This will check if the value is present in this range 'A' | 'a' | '0' and 'Z' | 'z' | '9'
+        
+        return (((int)'A' <= (int) c && (int) c <= (int)'Z') ||
+                ((int)'a' <= (int) c && (int) c <= (int)'z') ||
+                ((int)'0' <= (int) c && (int) c <= (int)'9')
+          );
+    }
+}
+
 // Solution 1 - Runtime 710 ms
 class Solution {
     public boolean isPalindrome(String s) {
