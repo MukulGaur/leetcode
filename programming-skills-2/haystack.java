e6b1fc62956ed5d2d242280d9f2a3f23eeb912ad
Mukul Gaur <mukulgaur91@gmail.com>
2022-10-24 23:31:55 +0530

    Create haystack.java
    Runtime: 0 ms
    Memory Usage: 39.8 MB

---
+++ programming-skills-2/haystack.java
@@ -0,0 +1,8 @@
+// 28. Find the Index of the First Occurrence in a String
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        return haystack.indexOf(needle);
+        
+    }
+}
