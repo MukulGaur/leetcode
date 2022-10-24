d04f59ee2dfc055d184da4afeb64391c48b0fbfd
Mukul Gaur <mukulgaur91@gmail.com>
2022-10-24 23:29:18 +0530

    Create monotonic-array.java
    Runtime: 1 ms
    Memory Usage: 93.6 MB

---
+++ programming-skills-2/monotonic-array.java
@@ -0,0 +1,24 @@
+// 896. Monotonic Array
+// TC - O(n)
+// SC - O(1)
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        
+        if(nums[0]<=nums[nums.length-1]){
+            for(int i=1; i<nums.length; i++){
+                if(nums[i]<nums[i-1])
+                    return false;
+            }
+        }
+        
+        else{
+            for(int i=1; i<nums.length; i++){
+                if(nums[i]>nums[i-1])
+                    return false;
+            }
+        }
+        
+        return true;
+        
+    }
+}
