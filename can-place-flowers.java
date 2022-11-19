60d9cebdb2f57c01096c03d0283a20f14f27e057
Mukul Gaur <mukulgaur91@gmail.com>
2022-11-19 23:48:43 +0530

    605. Can Place Flowers

---
+++ can-place-flowers.java
@@ -0,0 +1,16 @@
+// 605. Can Place Flowers
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int size = flowerbed.length;
+        if(n==0) return true;
+        for(int i=0; i<size; i++){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==size-1 || flowerbed[i+1]==0)){
+                n--;
+                if(n==0) return true;
+                flowerbed[i]=1;
+            }
+        }
+        return false;
+    }
+}
